712f23a6a1effb1d38b4bdbad10c7fa9e412abfd
kiehlb <thekiehlb@gmail.com>
2022-01-18 09:33:22 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {jsx, css} from '@emotion/react'
+import * as d3 from 'd3'
+import Nav from './components/Nav'
+import {Body, MainNav, RightBar, LeftBar} from './components/View/index'
+import RightSide from './components/RightSide'
+import LeftSide from './components/LeftSide'
+import React, {useRef, useEffect, useState} from 'react'
+
+function App() {
+  return (
+    <div>
+      <div>hello</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/background/index.ts
@@ -0,0 +1,5 @@
+import {Background} from './background'
+import {InitActivityLogger} from './logger'
+
+Background()
+InitActivityLogger()
