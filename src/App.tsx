31861810f3765bd065bc9f27646992f89fce6fb0
kiehlb <thekiehlb@gmail.com>
2022-02-08 14:59:56 +0900

    implemented react 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import {jsx, css} from '@emotion/react'
-import * as d3 from 'd3'
-import Nav from './components/Nav'
-import {Body, MainNav, RightBar, LeftBar} from './components/View/index'
-import RightSide from './components/RightSide'
-import LeftSide from './components/LeftSide'
-import React, {useRef, useEffect, useState} from 'react'
-
 function App() {
   return (
     <div>
