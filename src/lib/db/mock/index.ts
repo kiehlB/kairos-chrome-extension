0b5cba2aff77e26b4a6bf00eed7141c8b7ee2388
kiehlb <thekiehlb@gmail.com>
2022-03-16 14:11:03 +0900

    update

--- src/lib/db/mock/index.ts
+++ src/lib/db/mock/index.ts
@@ -1,4 +1,5 @@
 import Dexie from 'dexie';
+import { minusDays, setMidnight } from '../../../utils/dateUtils';
 import { Activity, Domain, RawActivity } from '../models/activity';
 import { DefiniteTimeRange, TimeRange } from '../models/time';
 
@@ -10,192 +11,161 @@ import {
   TitleTableRecord,
 } from '../types';
 
-import { Mock } from './Mock';
-import D from './data.json';
-
+import DATA from './data.json';
 import { createUrl, exportTableRecords, generateRecords } from './utils';
 
-const DATABASE_NAME = 'db';
+type Range = [number, number];
+const ACTIVITY_DURATION_RANGE_SHORT: Range = [100, 120000]; // 100ms ~ 2min
+const ACTIVITY_DURATION_RANGE_MEDIUM: Range = [120000, 300000]; // 2 ~ 5min
+const ACTIVITY_DURATION_RANGE_LONG: Range = [300000, 900000]; // 5 ~ 15min
+const IDLE_DURATION_RANGE: Range = [60000, 1800000]; // 1 ~ 30min
+const ACTIVITY_DURATION_PER_DAY_RANGE: Range = [1800000, 36000000]; // 0.5 ~ 10h
+const ACTIVITY_TIME_RANGE: Range = [32400000, 75600000]; // 9am ~ 9pm
+const DAYS_TO_GENERATE = 60; // 2 months
+
+const generateId: () => number = (function () {
+  const idGenerator = (function* getActivityIdGenerator() {
+    let index = 1;
+    while (true) yield index++;
+  })();
+
+  return function (): number {
+    return idGenerator.next().value;
+  };
+})();
+
+function randomRangeValue([min, max]) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomArrayElement<T>(arr: T[]): T {
+  const len = arr.length;
+  if (len === 0) {
+    throw new Error('Array is empty');
+  }
+  return arr[Math.floor(Math.random() * len) % len];
+}
+
+function generateActivityRecord(startTime: number, endTime: number) {
+  const website: any = randomArrayElement((DATA as any).activity as any);
+
+  return {
+    domain: website.domain,
+    path: website.path,
+    endTime,
+    startTime,
+  };
+}
+
 export const ACTIVITY_TABLE = 'activity';
 export const DOMAIN_TABLE = 'domain';
 export const TITLE_TABLE = 'title';
 
-export class MockDatabase extends Dexie implements DatabaseService {
-  private [ACTIVITY_TABLE]: Dexie.Table<ActivityTableRecord, number>;
-  private [DOMAIN_TABLE]: Dexie.Table<DomainTableRecord, number>;
-  private [TITLE_TABLE]: Dexie.Table<TitleTableRecord, number>;
+export class MockDatabase {
+  private activityRecords: Activity[];
+  private domainRecords: Record<string, Domain>;
 
   public constructor() {
-    super(DATABASE_NAME);
-    this.version(1).stores({
-      [ACTIVITY_TABLE]: '++id, domain, startTime, endTime',
-      [DOMAIN_TABLE]: 'id',
-      [TITLE_TABLE]: 'id',
-    });
+    const { activity, domain } = this.generateRecord();
 
-    this[ACTIVITY_TABLE] = this.table(ACTIVITY_TABLE);
-    this[DOMAIN_TABLE] = this.table(DOMAIN_TABLE);
-    this[TITLE_TABLE] = this.table(TITLE_TABLE);
+    this.activityRecords = activity;
+    this.domainRecords = domain;
   }
 
-  private async getFavIconUrlMap(): Promise<Record<string, string>> {
-    const domains = await this[DOMAIN_TABLE].toCollection().toArray();
-    return domains.reduce(
-      (acc: Record<string, string>, domain: DomainTableRecord) => {
-        acc[domain.id] = domain.favIconUrl;
-        return acc;
-      },
-      {}
-    );
-  }
-
-  private async getTitleMap(): Promise<Record<string, string>> {
-    const titles = await this[TITLE_TABLE].toCollection().toArray();
-    return titles.reduce(
-      (acc: Record<string, string>, title: TitleTableRecord) => {
-        acc[title.id] = title.title;
-        return acc;
-      },
-      {}
-    );
-  }
-
-  public async createActivityRecord(rawActivity: RawActivity): Promise<void> {
-    if (rawActivity.startTime >= rawActivity.endTime) {
-      new Error(`[db]: Invalid time range, ${JSON.stringify(rawActivity)}`);
-    }
-
-    const { activity, domain, title } = generateRecords(rawActivity);
-    try {
-      await this.transaction(
-        'rw',
-        [this[ACTIVITY_TABLE], this[DOMAIN_TABLE], this[TITLE_TABLE]],
-        async () => {
-          await this[ACTIVITY_TABLE].add(activity);
-
-          // Ensure that we don't overwrite existing domain favIconUrls with ""
-          if (
-            domain.favIconUrl !== '' ||
-            (await this[DOMAIN_TABLE].get({ id: domain.id })) === undefined
-          ) {
-            await this[DOMAIN_TABLE].put(domain);
-          }
-
-          // Ensure that we don't overwrite existing URL titles with ""
-          if (
-            title.title !== '' ||
-            (await this[TITLE_TABLE].get({ id: title.id })) === undefined
-          ) {
-            await this[TITLE_TABLE].put(title);
-          }
-        }
-      );
-    } catch (err) {
-      new Error(err);
-    }
+  public async createActivityRecord(data): Promise<void> {
+    throw new Error('Mock database does not support creating records');
   }
 
   public deleteActivityRecords(recordIds: number[]): Promise<void> {
     return this[ACTIVITY_TABLE].bulkDelete(recordIds);
   }
 
-  // public fetchAllActivityRecords(): Promise<Activity[]> {
-  //   return this.fetchActivityRecords({ start: null, end: null });
-  // }
+  public async fetchAllActivityDomains() {
+    return new Promise((resolve) => {
+      resolve(this.domainRecords);
+    });
+  }
+
+  public fetchAllActivityRecords(): Promise<Activity[]> {
+    return this.fetchActivityRecords({ start: null, end: null });
+  }
+
+  public fetchActivityRecords({
+    start: startTime,
+    end: endTime,
+  }: TimeRange): Promise<Activity[]> {
+    return new Promise((resolve) => {
+      let result;
+      if (startTime !== null && endTime !== null) {
+        result = this.activityRecords.filter(
+          (datum) => datum.startTime >= startTime && datum.endTime <= endTime
+        );
+      } else if (startTime !== null) {
+        result = this.activityRecords.filter(
+          (datum) => datum.startTime >= startTime
+        );
+      } else if (endTime !== null) {
+        result = this.activityRecords.filter(
+          (datum) => datum.endTime <= endTime
+        );
+      } else {
+        result = this.activityRecords;
+      }
+      resolve(result);
+    });
+  }
 
   public fetchActivityTimeRange(): Promise<DefiniteTimeRange | null> {
-    return this[ACTIVITY_TABLE].orderBy('startTime')
-      .first()
-      .then((oldestRecord) => {
-        return !oldestRecord
-          ? null
-          : {
-              start: oldestRecord.startTime,
-              end: Date.now(),
-            };
-      });
-  }
+    return new Promise((resolve) => {
+      const lastIndex = this.activityRecords.length - 1;
+      const start = this.activityRecords?.[lastIndex].startTime;
 
-  public async exportDatabaseRecords(): Promise<DatabaseRecords> {
-    const activityTableRecords = (
-      await exportTableRecords<ActivityTableRecord>(this[ACTIVITY_TABLE])
-    ).map((record) => {
-      delete record.id;
-      return record;
-    });
-    const domainTableRecords = await exportTableRecords<DomainTableRecord>(
-      this[DOMAIN_TABLE]
-    );
-    const titleTableRecords = await exportTableRecords<TitleTableRecord>(
-      this[TITLE_TABLE]
-    );
-
-    return {
-      [ACTIVITY_TABLE]: activityTableRecords,
-      [DOMAIN_TABLE]: domainTableRecords,
-      [TITLE_TABLE]: titleTableRecords,
-    };
-  }
-
-  public async fetchActivityRecords(): Promise<Activity[]> {
-    const a = new Mock();
-
-    const records = (a as any).activityRecords;
-
-    const [favIconUrlMapByDomain, titleMapByUrl] = await Promise.all([
-      this.getFavIconUrlMap(),
-      this.getTitleMap(),
-    ]);
-
-    return records.map((activity: ActivityTableRecord): Activity => {
-      const url = createUrl(activity);
-
-      return {
-        ...activity,
-        // favIconUrl: favIconUrlMapByDomain[activity.domain] || '',
-        // title: titleMapByUrl[url] || '',
-        // url,
-      } as Activity;
+      resolve(start ? { start, end: Date.now() } : null);
     });
   }
 
-  public async fetchAllActivityDomains(): Promise<Record<string, Domain>> {
-    const a = new Mock();
+  public generateRecord() {
+    const domain = (DATA as any).domain.reduce((acc, datum) => {
+      acc[datum.id] = {
+        id: datum.id,
+        favIconUrl: datum.favIconUrl,
+      };
+      return acc;
+    }, {});
 
-    const domains = await (a as any).domainRecords;
+    const activity: Activity[] = [];
+    const startOfToday = setMidnight();
+    for (let day = 0; day < DAYS_TO_GENERATE; day++) {
+      const startOfDay = minusDays(startOfToday, day);
+      const browsingStartTime = startOfDay + ACTIVITY_TIME_RANGE[0];
+      const browsingEndTime =
+        startOfDay + randomRangeValue([32400000, 75600000]);
 
-    const intoArray = Object.values(domains);
+      let maxDuration = randomRangeValue([1800000, 36000000]);
+      let endTime = browsingEndTime;
+      while (maxDuration > 0 && endTime > browsingStartTime) {
+        const durationRange = randomArrayElement([
+          ACTIVITY_DURATION_RANGE_LONG,
+          ACTIVITY_DURATION_RANGE_MEDIUM,
+          ACTIVITY_DURATION_RANGE_SHORT,
+        ]);
+        const duration = randomRangeValue(durationRange as any);
+        const startTime = endTime - duration;
+        const activityRecord = generateActivityRecord(startTime, endTime);
 
-    return (intoArray as any).reduce(
-      (acc: Record<string, Domain>, domain: DomainTableRecord) => {
-        acc[domain.id] = domain;
-        return acc;
-      },
-      {}
-    );
-  }
+        activity.push(activityRecord as any);
+        endTime = startTime - 1;
+        maxDuration = maxDuration - duration;
 
-  public async importDatabaseRecords(data: DatabaseRecords): Promise<void> {
-    const a = (await this.fetchActivityRecords()) as any;
-    const b = (await this.fetchAllActivityDomains()) as any;
-
-    try {
-      await this.transaction(
-        'rw',
-        [this[ACTIVITY_TABLE], this[DOMAIN_TABLE], this[TITLE_TABLE]],
-        async () => {
-          await this[ACTIVITY_TABLE].clear();
-          await this[DOMAIN_TABLE].clear();
-          await this[TITLE_TABLE].clear();
-
-          await this[ACTIVITY_TABLE].bulkAdd(a);
-          await this[DOMAIN_TABLE].bulkAdd(D[DOMAIN_TABLE]);
-          await this[TITLE_TABLE].bulkAdd(D[TITLE_TABLE]);
+        // Add some idle time between each activity
+        if (Math.random() > 0.5) {
+          const idleDuration = randomRangeValue([60000, 1800000]);
+          endTime = endTime - idleDuration - 1;
+          maxDuration = maxDuration - idleDuration;
         }
-      );
-      console.log('[db] All records were imported successfully');
-    } catch (err) {
-      console.error('[db]', err);
+      }
     }
+
+    return { activity, domain };
   }
 }
